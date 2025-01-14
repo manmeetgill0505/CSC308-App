38851da4e51563e0f06bd0e430349d448377341f
Manmeet Gill <manmeetg2124@gmail.com>
2025-01-14 10:50:58 -0800

    defined /users to go beyond /

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -11,10 +11,45 @@ const port = 8000;
 app.use(express.json());
 //standard configuration so in JSON format
 
+const users = {
+	users_list: [
+	{
+		id: "xyz789",
+		name: "Charlie",
+		job: "Janitor"
+	},
+	{
+		id: "abc123",
+		name: "Mac",
+		job: "Bouncer"
+	},
+	{
+		id: "ppp222",
+		name: "Mac",
+		job: "Professor"
+	},
+	{
+		id: "yat999",
+		name: "Dee",
+		job: "Aspiring actress"
+	},
+	{
+		id: "zap555",
+		name: "Dennis",
+		job: "Bartender"
+	}]
+};
+//We defined list of users and theri random ids (pairs and keys)
+
 app.get("/", (req, res) => {
 	res.send("Hello World!");
 });
 //Setting up the first API endpoint (sends a plain message for now)
+
+app.get("/users", (req, res) => {
+	res.send(users);
+});
+//sends users if users are requested
 
 app.listen(port, () => {
 	console.log(
