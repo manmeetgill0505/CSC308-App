32a2349e957220d552cefa616ba3259c19b0500b
Manmeet Gill <manmeetg2124@gmail.com>
2025-01-22 11:18:36 -0800

    The submit button and POST calls to add users

--- node_modules/react-frontend/src/MyApp.jsx
+++ node_modules/react-frontend/src/MyApp.jsx
@@ -13,7 +13,11 @@ function MyApp() {
 	}
 	
 	function updateList(person) {
-		setCharacters([...characters, person]);
+		postUser(person)
+			.then(() => setCharacters([...characters, person]))
+			.catch((error) => {
+				console.log(error);
+			})
 	}
 	
 	function fetchUsers() {
@@ -27,6 +31,17 @@ function MyApp() {
 			.then((json) =>setCharacters(json["users_list"]))
 			.catch((error) => {console.log(error);});
 	},	[]	);
+	
+	function postUser(person) {
+		const promise = fetch("http://localhost:8000/users", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(person),
+		});
+		return promise;
+	}
 	
 	return (
 			<div className="container">
